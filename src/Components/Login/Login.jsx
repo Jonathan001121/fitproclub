c8e708f110fd57b24e8bb98b8093b64fed251e8d
chesterchan <chesterchan@beNovelty.com>
2024-03-12 20:58:55 +0800

    Login updates _

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
   return (
     <div className="login-container">
       <div className="login-form">
-        <h2 className="login-heading">Log in to FitProClub</h2>
+        <h2 className="login-heading">Welcome to SILFIS</h2>
         <form>
           <input type="text" placeholder="Username" className="login-input" />
           <input type="password" placeholder="Password" className="login-input" />
