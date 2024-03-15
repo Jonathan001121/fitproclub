332ed932968373f579441768c0e96ece1e378394
chesterchan <chesterchan@beNovelty.com>
2024-03-15 20:08:25 +0800

    updates on Dashboards and MyCourse

--- src/Components/MyCourse/MyCourse.jsx
+++ src/Components/MyCourse/MyCourse.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import CourseCard from '../CourseCard/CourseCard'
 import Navnode from '../Navnode/Navnode'; 
-
+import "./MyCourse.css"
    <Navnode /> 
 const MyCourse= () => {
   return (
