ec0e435f9fbf5888c5f2d0d35deb6202b1a605f4
chesterchan <chesterchan@beNovelty.com>
2024-03-18 04:45:37 +0800

    Wow, homepage model squat first render

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,11 +5,21 @@ import bmi from  "../../assets/icons/bmi.png"
 import increase from  "../../assets/icons/increase.png"
 import 'charts.css'
 import "./Home.css"
+import { Canvas } from "@react-three/fiber";
+import { Exp } from "../Exp";
+
+
 import Navnode from '../Navnode/Navnode'; 
+//https://www.youtube.com/watch?v=pGMKIyALcK0&t=885s&ab_channel=WawaSensei
+
 const Home = () => {
   return (
     <div className="home">
-           <Navnode /> 
+      <Canvas shadows camera={{ position: [0, 2, 5], fov: 30 }}>
+       {/* <color attach="background" args={["#ececec"]} /> */}
+      <Exp></Exp>
+    </Canvas>
+ 
         </div>
 
     );
