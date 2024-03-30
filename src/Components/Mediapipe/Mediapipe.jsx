af78fbfc4b797c371c340e558f982e4aa1756f99
Jonathan001121 <20202288@life.hkbu.edu.hk>
2024-03-31 03:28:30 +0800

    clock

--- src/Components/Mediapipe/Mediapipe.jsx
+++ src/Components/Mediapipe/Mediapipe.jsx
@@ -9,7 +9,39 @@ import axios from 'axios';
 import { Alert, AlertTitle , Collapse} from '@mui/material';
 import ExerciseDialog from '../ExerciseDialog/ExerciseDialog';
 import useCursor from "../elderly_cursor";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+import { Margin } from '@mui/icons-material';
+import Button from '@mui/material/Button';
 const Mediapipe = () => {
+  const [showImage, setShowImage] = useState(false);
+  const [select, setSelect] = useState(10);
+  const [isStart,setisStart]=useState(false);
+  const startTimer = () => {
+    
+    setTimeout(() => {
+      setShowImage(true);
+    }, 10000);
+
+  };
+  const handleStart = () => {
+    setisStart(true);
+    startTimer();
+  }
+const renderTime = ({ remainingTime }) => {
+
+  if (remainingTime === 0) {
+    setShowImage(true);
+    
+  }
+
+  return (
+    <div className="timer">
+      <div className="text">Remaining</div>
+      <div className="value">{remainingTime}</div>
+      <div className="text">seconds</div>
+    </div>
+  );
+};
      // cursor
   const { cursor, changePosition } = useCursor();
    
@@ -58,6 +90,7 @@ const Mediapipe = () => {
       <Alert style ={{"position": "absolute", "margin-left":"20%","width":"50%", "padding":0}}severity='error'>
 
           <AlertTitle > Alert</AlertTitle>
+
         
           <div style={{"display":"flex", "flex-direction":"row","justify-content":"space-between","width":"100%", "margin-left": "0"}}  > 
           <p>  No Error Detected</p>
@@ -68,9 +101,25 @@ const Mediapipe = () => {
         
         </Alert>
       <div class="overlayContainer">
+  
    
-        <img id="videoFeed" class="videoFeed" src={`http://127.0.0.1:8000/${path}`} />
-   
+      {showImage ? (
+        <img id="videoFeed" className="videoFeed" src={`http://127.0.0.1:8000/${path}`} />
+      ) : (
+        <div className="videoFeed" >
+          <Button variant="contained" onClick={handleStart}>Start</Button>
+          <CountdownCircleTimer
+            isPlaying={isStart}
+            duration={select}
+            colors={[['#004777', 0.33], ['#F7B801', 0.33], ['#A30000']]}
+            onComplete={() => [false, 1000]}
+          >
+            {renderTime}
+          </CountdownCircleTimer>
+        </div>
+      )}
+        
+
         <div class="textInfo">
    
          
--- src/Components/MyCourse/MyCourse.jsx
+++ src/Components/MyCourse/MyCourse.jsx
@@ -3,12 +3,15 @@ import CourseCard from '../CourseCard/CourseCard'
 import Navnode from '../Navnode/Navnode'; 
 import "./MyCourse.css"
 import useCursor from "../elderly_cursor";
-   <Navnode /> 
+
 const MyCourse= () => {
   const { cursor, changePosition } = useCursor();
   return (
+    
     <div className="Course" onMouseMove={changePosition}>
+  
          <div className="cursor-style" ref={cursor} ></div>
+      
         <CourseCard />
         <Navnode /> 
     </div>
